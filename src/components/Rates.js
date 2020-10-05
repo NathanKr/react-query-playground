9825cebd6ae953f5cfc2c1ce2e536af61e8198ec
Nathan Krasney <natankrasney@gmail.com>
2020-10-05 12:58:41 +0300

    initial version

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import {useQuery} from 'react-query'
+import React from "react";
+import { useQuery } from "react-query";
 
-const Rates = () =>{
-    const queryKey = "latestRate";
-    const latestRateHandler =   
-    {} = useQuery()
-    return <div>Rates</div>
-}
+const Rates = () => {
+  const queryKey = "latestRate";
+  const latestRateQueryHandler = async () => {
+    const response = await fetch("https://api.ratesapi.io/api/latest");
+    const data = await response.json();
+    return data;
+  };
+  const { status, data, error } = useQuery(queryKey, latestRateQueryHandler);
+
+  console.log(`status : ${status}`);
+  console.log(`data : ${JSON.stringify(data)}`);
+  console.log(`error : ${error}`);
+
+  if (status === "loading") {
+    return <div>Loading ...</div>;
+  }
+
+  if (error) {
+    return <div>{JSON.stringify(error)}</div>;
+  }
+
+  return <div>1 EUR is {data.rates.USD} USD</div>;
+};
 
 export default Rates;
